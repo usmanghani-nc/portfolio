a597cfa61d30887c0c8e7c58379c250bf70db7af
usman-at-714572130185 <afro123737@gmail.com>
2023-03-14 21:51:32 +0500

    update

--- src/container/about/index.tsx
+++ src/container/about/index.tsx
@@ -9,15 +9,14 @@ export default function About({}) {
           <h2 className="text-primary-text font-bold text-2xl md:text-4xl md:mb-5">About me 🤙</h2>
 
           <p className="text-secondary-text w-[50%] mx-auto text-[18px] md:mb-5">
-            Hi folks, My name is Muhammad Usman Ghani i live in Pakistan, Karachi city of where you
-            find all kind of good food. For past 3 years i`ve been working as a Sofware Engineer and
-            building good things around Javascript.
+            Hi folks, My name is Muhammad Usman Ghani i live in Pakistan. For past 4 years i have
+            been working as a Sofware Engineer and and building all good things around Javascript.
           </p>
 
-          <p className="text-secondary-text text-[18px] md:mb-2">
+          {/* <p className="text-secondary-text text-[18px] md:mb-2">
             Fast-forward to today, and I’ve had the privilege of working at a start-up, a huge
             corporation.
-          </p>
+          </p> */}
         </div>
       </div>
     </Page>
