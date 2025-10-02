00af887316f3cf90e1767cf907dc645e015a1045
usmanghanidev <usman.ghani@nymcard.com>
2025-10-02 11:17:08 +0500

    Refactor RasumeBtn component to simplify button structure and improve readability.

--- src/components/rasume-btn/index.tsx
+++ src/components/rasume-btn/index.tsx
@@ -4,9 +4,7 @@ interface Props {}
 
 const RasumeBtn: React.FC<Props> = ({}) => {
   return (
-    <button
-      className={`bg-brand hover:bg-brand-hover transition text-white py-2 px-4 font-bold rounded-full`}
-    >
+    <button className="bg-brand hover:bg-brand-hover transition text-white py-2 px-4 font-bold rounded-full">
       <a href="/usmanghani.pdf" download="muhammad-usman-ghani-resume">
         Download Resume
       </a>
